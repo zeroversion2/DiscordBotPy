5fb53f310ce9890f4796e7f9c02d2f38b920ba1e
Zero <zerotwofan@gmail.com>
2021-01-09 22:43:14 -0800

    add new command

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import json
 import typing
 from system import System
 import shell
+from PIL import Image
+import requests
+import io
+import random
 
 config = open("config.json")
 config_data = json.load(config)
@@ -47,6 +51,27 @@ async def ping(ctx, *, member: discord.Member):
 
 
 @bot.command()
+async def cum(ctx, member: discord.Member):
+    random.seed()
+    pic_to_cum = Image.open(requests.get(member.avatar_url_as(format='png', size=512), stream=True).raw)
+
+    cum_images = [
+        "https://cdn.discordapp.com/attachments/609378055441612819/619049148062367773/cum-splatter-png-2.png",
+        "https://cdn.discordapp.com/attachments/609378055441612819/626884316215115807/fake-cum-png-5-png-image-cum"
+        "-transparent-background-369_312.png",
+        "https://cdn.discordapp.com/attachments/609378055441612819/626884331322998788/cum-splatter-png-2-300x200.png"]
+
+    cum_image = Image.open(requests.get(cum_images[random.randrange(0, 2)], stream=True).raw)
+    region = cum_image.resize((round(pic_to_cum.size[0] * 7/8), round(pic_to_cum.size[1] * 7/8)))
+    pic_to_cum.paste(region, (round(pic_to_cum.size[0] * 1/8), round(pic_to_cum.size[1] * 1/8)), region)
+    image_binary = io.BytesIO()
+    pic_to_cum.save(image_binary, 'PNG')
+    image_binary.seek(0)
+
+    await ctx.send(f"you just got CUMMED on BITCH, <@{member.id}> !!!", file=discord.File(fp=image_binary, filename='cum-image.png'))
+
+
+@bot.command()
 @commands.is_owner()
 async def ban(ctx, members: commands.Greedy[discord.Member],
               delete_days: typing.Optional[int] = 0, *, reason=None):
@@ -57,7 +82,7 @@ async def ban(ctx, members: commands.Greedy[discord.Member],
 
 @bot.event
 async def on_command_error(ctx, error):
-    await ctx.send(error)
+    print(error)
 
 bot.add_cog(System(bot))
 
